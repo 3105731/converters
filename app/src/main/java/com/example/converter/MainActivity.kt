2eb60e248ef442922b0e8860a475bd7268c02e64
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-07-08 15:26:31 +0300

    update ui and code quality

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -2,46 +2,64 @@ package com.example.converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Spinner
+import android.widget.Toast
 import com.example.converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        addcallbacks()
+        addCallbacks()
     }
 
-    private fun addcallbacks() {
+    private fun addCallbacks() {
         binding.reset.setOnClickListener {
             clearInput()
             clearOutput()
         }
 
         binding.submit.setOnClickListener {
-            val num = binding.Enter.text.toString().toInt()
-            val bin = Integer.toBinaryString(num)
-            val oct = Integer.toOctalString(num)
-            val hex = Integer.toHexString(num)
 
-            binding.Bin.setText(bin)
-            binding.Oct.setText(oct)
-            binding.Hex.setText(hex)
-            binding.Des.setText(num.toString())
+            val num = binding.Enter.text.toString()
+
+            var dec = 0
+            var bin = ""
+            var oct = ""
+            var hex = ""
+
+            try {
+                dec = Integer.parseInt(num)
+                bin = Integer.toBinaryString(dec)
+                oct = Integer.toOctalString(dec)
+                hex = Integer.toHexString(dec)
+            }
+            catch (e: NumberFormatException) {
+                clearOutput()
+                Toast(this).apply {
+                    duration = Toast.LENGTH_SHORT
+                    setText("Max Length Exceeded")
+                    show()
+                }
+                return@setOnClickListener
+            }
+
+
+            binding.Bin.text = bin
+            binding.Oct.text = oct
+            binding.Hex.text = hex
+            binding.Des.text = dec.toString()
         }
     }
 
 
+    private fun clearInput() {
+        binding.Enter.setText("")
+    }
 
-
-
-        fun clearInput() {
-            binding.Enter.setText("")
-        }
-    fun clearOutput(){
+    private fun clearOutput() {
         binding.Des.text = ""
         binding.Bin.text = ""
         binding.Oct.text = ""
